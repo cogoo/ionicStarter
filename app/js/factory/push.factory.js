7bcc298c836841bccab8a844252c3a40a348f431
Colin Ogoo <c_ogoo@Colins-MacBook-Pro.local>
2016-10-16 01:36:58 +0100

    push dev branch

--- app/js/factory/push.factory.js
+++ app/js/factory/push.factory.js
@@ -14,7 +14,7 @@ function PushFactory($http) {
       method: 'POST',
       url: 'https://api.ionic.io/push/notifications',
       headers: {
-        'Authorization': 'Bearer <insert bearer token>',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3MDk2YmJiNi02ZDg5LTQ4MDgtODAxOS0zYzYyOGQ2Y2JhMTYifQ.b_u4RGY-wcXyt7442wnXsi8tQSSEP_kkGyGME-TTpDA',
         'Content-Type': 'application/json'
       },
       data: {
